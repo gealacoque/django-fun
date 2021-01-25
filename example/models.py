c61d0b9dc0aefad1306147c4803064bd30677dd9
Geórgia Alacoque <ge.alacoque@gmail.com>
2021-01-25 17:19:28 -0300

    Aletrações do tutorial.

--- example/models.py
+++ example/models.py
@@ -1,6 +1,52 @@
 from django.db import models
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=30)
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    first_name = models.CharField("person's first name", max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=None, blank=True)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, blank=True)
+
+
+class Musician(Person):
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    publications = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ['headline']
+
+    def __str__(self):
+        return self.headline
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pokemaoc/', include('pokemaoc.urls'))
+    url(r"^", include("users.urls")),
+    path('pokemaoc/', include('pokemaoc.urls')),
 ]
